7f1bb31d92965be88b5f28b786bf971cc9d2ebf6
weti-hb <157122452+weti-hb@users.noreply.github.com>
2025-01-09 17:53:40 +0100

    Update main.tf

--- qdrant/terraform/modules/cluster/main.tf
+++ qdrant/terraform/modules/cluster/main.tf
@@ -45,7 +45,7 @@ module "qdrant_cluster" {
   initial_node_count            = 1
   kubernetes_version            = "latest"
   release_channel               = "RAPID"
-  stateful_ha                   = false
+  stateful_ha                   = true
 
   # cluster_resource_labels section assigns labels to the cluster resources for identification and management purposes
   cluster_resource_labels = {
